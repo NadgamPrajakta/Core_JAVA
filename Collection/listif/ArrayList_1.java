f47e0b9a09c92852f6102fae0ba273851112a451
NadgamPrajkta <nadgamprajkta@gmail.com>
2025-12-25 19:52:07 +0530

    List Interface

---
+++ Collection/listif/ArrayList_1.java
@@ -0,0 +1,27 @@
+package listif;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayList_1 {
+
+	public static void main(String[] args) {
+		
+		List<String> list = new ArrayList<String>();
+		
+		list.add("ABC");
+		list.add("DEF");
+		list.add("GHI");
+		list.add("JKL");
+		list.add("MNO");
+		list.add("PQR");
+		list.add("STU");
+		list.add("VWT");
+		list.add("XYZ");
+		list.add("SOP");
+		
+		System.out.println(list);
+
+	}
+
+}
