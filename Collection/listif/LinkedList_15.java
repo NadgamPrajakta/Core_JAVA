a91cbfc95fdbc666d6ae46539e1db28ff42ea322
NadgamPrajkta <nadgamprajkta@gmail.com>
2025-12-25 20:11:41 +0530

    List Interface

---
+++ Collection/listif/LinkedList_15.java
@@ -0,0 +1,13 @@
+package listif;
+import java.util.*;
+
+public class LinkedList_15 {
+	
+    public static void main(String[] args) {
+    	
+        LinkedList<String> list = new LinkedList<>(Arrays.asList("A","B"));
+        
+        list.clear();
+        System.out.println(list);
+    }
+}
