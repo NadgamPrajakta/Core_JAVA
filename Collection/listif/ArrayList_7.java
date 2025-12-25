fdea2fa47be03fe79e441a2605350217e5b2eed1
NadgamPrajkta <nadgamprajkta@gmail.com>
2025-12-25 19:56:49 +0530

    List Interface

---
+++ Collection/listif/ArrayList_7.java
@@ -0,0 +1,21 @@
+package listif;
+import java.util.*;
+
+public class ArrayList_7 {
+	
+	    public static void main(String[] args) {
+	    	
+	        ArrayList<String> list = new ArrayList<>();
+	        
+	        System.out.println("Is Empty? " + list.isEmpty());
+	        
+	        list.add("Hello");
+	        list.add("Hiii");
+	        list.add("Bye");
+	        list.add("Welcome");
+	        list.add("Java");
+	        
+	        System.out.println("Is Empty? " + list.isEmpty());
+	    }
+	}
+
