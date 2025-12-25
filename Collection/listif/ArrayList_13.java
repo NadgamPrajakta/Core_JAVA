c4133c02004207533bb3c04747c947bf29615519
NadgamPrajkta <nadgamprajkta@gmail.com>
2025-12-25 20:01:45 +0530

    List Interface

---
+++ Collection/listif/ArrayList_13.java
@@ -0,0 +1,14 @@
+package listif;
+import java.util.*;
+
+public class ArrayList_13 {
+	
+    public static void main(String[] args) {
+    	
+        ArrayList<String> list = new ArrayList<>(Arrays.asList("X", "Y", "Z", "P", "M", "N", "S", "U"));
+        
+        list.clear();
+        
+        System.out.println(list);
+    }
+}
