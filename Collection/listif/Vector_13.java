b24c3e1c7b9c2327224394b41dd3111653f19c91
NadgamPrajkta <nadgamprajkta@gmail.com>
2025-12-25 20:25:51 +0530

    List Interface

---
+++ Collection/listif/Vector_13.java
@@ -0,0 +1,13 @@
+package listif;
+import java.util.*;
+
+public class Vector_13 {
+	
+    public static void main(String[] args) {
+    	
+        Vector<String> v = new Vector<String>(Arrays.asList("Sun","Moon","Star"));
+        
+        for(String s : v)
+            System.out.println(s);
+    }
+}
