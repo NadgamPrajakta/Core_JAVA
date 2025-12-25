795834596f714850bce0f259edf75649d0993f4b
NadgamPrajkta <nadgamprajkta@gmail.com>
2025-12-25 20:15:43 +0530

    List Interface

---
+++ Collection/listif/Stack_8.java
@@ -0,0 +1,25 @@
+package listif;
+import java.util.*;
+
+public class Stack_8 {
+	
+    public static void main(String[] args) {
+    	
+        Stack<Integer> st = new Stack<Integer>();
+        
+        st.add(100);
+        st.add(200);
+        st.add(300);
+        st.add(400);
+        st.add(500);
+        st.add(700);
+        st.add(600);
+        st.add(800);
+        st.add(900);
+        st.add(1200);
+        st.add(3400);
+        st.add(222);
+        
+        System.out.println(st);
+    }
+}
