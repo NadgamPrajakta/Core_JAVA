e044b740f8a1c67185c4f033dc934c66176e544d
NadgamPrajkta <nadgamprajkta@gmail.com>
2025-12-25 20:22:18 +0530

    List Interface

---
+++ Collection/listif/Vector_6.java
@@ -0,0 +1,12 @@
+package listif;
+import java.util.*;
+
+public class Vector_6 {
+	
+    public static void main(String[] args) {
+    	
+        Vector<String> v = new Vector<>(Arrays.asList("A","B","C"));
+        
+        System.out.println(v.get(1));
+    }
+}
