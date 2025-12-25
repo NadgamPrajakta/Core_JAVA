f7e5b8b43d96b0128d4c8f4a80a14956492595a4
NadgamPrajkta <nadgamprajkta@gmail.com>
2025-12-25 19:59:30 +0530

    List Interface

---
+++ Collection/listif/ArrayList_10.java
@@ -0,0 +1,20 @@
+package listif;
+
+import java.util.*;
+
+public class ArrayList_10 {
+	
+    public static void main(String[] args) {
+    	
+        ArrayList<Integer> list = new ArrayList<>(Arrays.asList(1, 2, 3));
+        
+        Object[] arr = list.toArray();
+        
+        System.out.println(Arrays.toString(arr));
+    }
+}
+
+
+        
+    
+
