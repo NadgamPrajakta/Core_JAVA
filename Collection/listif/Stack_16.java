3d00d7b70c26a80d286c5757ee37c81974e568be
NadgamPrajkta <nadgamprajkta@gmail.com>
2025-12-25 20:20:09 +0530

    List Interface

---
+++ Collection/listif/Stack_16.java
@@ -0,0 +1,15 @@
+package listif;
+import java.util.*;
+
+public class Stack_16 {
+	
+    public static void main(String[] args) {
+    	
+        Stack<String> st = new Stack<String>();
+        
+        st.push("X");
+        st.push("Y");
+        st.clear();
+        System.out.println(st);
+    }
+}
