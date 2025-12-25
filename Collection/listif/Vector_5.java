98bb67d13c61b0109fcc8d22ab07be2558bde4c7
NadgamPrajkta <nadgamprajkta@gmail.com>
2025-12-25 20:21:47 +0530

    List Interface

---
+++ Collection/listif/Vector_5.java
@@ -0,0 +1,22 @@
+package listif;
+import java.util.*;
+
+public class Vector_5 {
+	
+    public static void main(String[] args) {
+    	
+        Vector<String> v = new Vector<String>();
+        
+        v.addElement("Red");
+        v.addElement("Blue");
+        v.addElement("Yellow");
+        v.addElement("Pink");
+        v.addElement("Black");
+        v.addElement("Green");
+        v.addElement("Blue");
+        v.addElement("Purple");
+        v.addElement("White");
+        
+        System.out.println(v);
+    }
+}
