4424e81757540a0342f247e03b48a50c5b30e86f
NadgamPrajkta <nadgamprajkta@gmail.com>
2025-12-25 20:03:49 +0530

    List Interface

---
+++ Collection/listif/LinkedList_1.java
@@ -0,0 +1,30 @@
+package listif;
+import java.util.*;
+
+public class LinkedList_1 {
+	
+    public static void main(String[] args) {
+    	
+        LinkedList<String> list = new LinkedList<>();
+        
+        list.add("Apple");
+        list.add("Mango");
+        list.add("Banana");
+        list.add("Orange");
+        list.add("Kiwi");
+        list.add("Blueberry");
+        list.add("Plum");
+        list.add("Raspberry");
+        list.add("Apricot");
+        list.add("Lychee");
+        list.add("Papaya");
+        list.add("Guava");
+        list.add("Pomegranate");
+        list.add("Avocado");
+        list.add("Dragon Fruit");
+        
+        
+        
+        System.out.println(list);
+    }
+}
