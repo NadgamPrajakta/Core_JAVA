6f62b4b108717d0b15dd98cc1489ab591689b94f
NadgamPrajkta <nadgamprajkta@gmail.com>
2025-12-25 20:20:56 +0530

    List Interface

---
+++ Collection/listif/Vector_3.java
@@ -0,0 +1,20 @@
+package listif;
+import java.util.*;
+
+public class Vector_3 {
+	
+    public static void main(String[] args) {
+    	
+        Vector<String> v = new Vector<String>();
+        
+        v.add("Apple");
+        v.add("Mango");
+        v.add("Banana");
+        v.add("Orange");
+        v.add("papaya");
+        v.add("Watermelon");
+        v.add("Kiwi");
+        
+        System.out.println(v);
+    }
+}
