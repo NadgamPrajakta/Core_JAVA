defae950e958218490796fede1ef21b78446dd5a
NadgamPrajkta <nadgamprajkta@gmail.com>
2026-01-11 23:49:06 +0530

    JDBC_API

---
+++ JDBC_API/jdbc_api/Oldest_Stud.java
@@ -0,0 +1,30 @@
+package jdbc_api;
+
+import java.sql.*;
+
+public class Oldest_Stud {
+	
+    public static void main(String[] args) {
+    	
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            
+            Connection con = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/jdbc1","root","12345");
+
+            PreparedStatement ps =
+                con.prepareStatement(
+                    "SELECT * FROM students ORDER BY age DESC LIMIT 1");
+
+            ResultSet rs = ps.executeQuery();
+
+            if (rs.next())
+                System.out.println(
+                    rs.getInt(1)+" "+rs.getString(2)+" "+rs.getInt(3));
+
+            con.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
