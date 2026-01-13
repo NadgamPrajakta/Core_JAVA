cab5deae3823821df55288128f70395bac7d0a92
NadgamPrajkta <nadgamprajkta@gmail.com>
2026-01-14 00:53:23 +0530

    jdbc_callable

---
+++ JDBC_API/callable_use/Third.java
@@ -0,0 +1,38 @@
+package callable_use;
+
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Types;
+
+public class Third {
+
+	public static void main(String[] args) throws Exception {
+         Class.forName("com.mysql.cj.jdbc.Driver");
+		
+		Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/user","root","12345");
+		
+
+		 CallableStatement call=con.prepareCall("{call getbyidname(?, ?)}");
+		 
+		 call.setInt(1,  2);
+		 call.setString(2, "def");
+		 
+		 ResultSet set = call.executeQuery();
+		 
+		 while(set.next()) {
+			 
+     		 System.out.println(set.getString(2));
+			 System.out.println(set.getInt(1));
+			 System.out.println("*******************");
+			 
+		 }
+			 
+		 con.close();
+		 call.close();
+		 set.close();
+		
+	}
+
+}
